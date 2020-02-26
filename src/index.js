2a5e32165bf5373393c4a0249a217c54301f9243
olexan7 <ghoostt61@gmail.com>
2020-02-27 01:37:27 +0400

    feat: complete task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length == 0) return 0;
+    let minElem = 100000000;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < minElem) minElem = array[i];
+    }
+    return minElem;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length == 0) return 0;
+    let maxElem = -100000000;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maxElem) maxElem = array[i];
+    }
+    return maxElem;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) return 0;
+    if (array.length == 0) return 0;
+    let avgElem = 0;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    avgElem = sum / array.length;
+    return avgElem;
+};
